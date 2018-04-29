66185067e668d92831787018f5eb7e5143449b91
Cody Sheridan <csheridan994@gmail.com>
2018-04-29 14:53:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function viewCart() {
       message = `${message}${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
       break;
     case 3:
-      message = `${message}${cart[2].itemName} at $${cart[2].itemPrice}.`;
+      message = `${message}${cart[0].itemName} at $${cart[0].itemPrice}, ${cart[1].itemName} at $${cart[1].itemPrice}, and ${cart[2].itemName} at $${cart[2].itemPrice}.`;
       break;
   }
   return message;
