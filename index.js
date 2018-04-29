d3096a9fdd16dae1ad69760d9c2113d5734a347d
Cody Sheridan <csheridan994@gmail.com>
2018-04-29 15:19:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function total() {
 }
 
 function removeFromCart(item) {
-   for (i = 0; i < cart.length; i++) {
+   for (var i = 0; i < cart.length; i++) {
       if(cart[i].itemName === item)
         cart.splice(i, 1);
         return cart;
