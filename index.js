48fe3c0e4052545d8e9aeb17e06165f9ef38f616
Cody Sheridan <csheridan994@gmail.com>
2018-04-29 15:39:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function total() {
 
 function removeFromCart(item) {
    for (var i = 0; i < cart.length; i++) {
-      if(cart[i].itemName === item) {
+      if (cart[i].itemName === item) {
         cart.splice(i, 0);
       } else {
       return "That item is not in your cart.";
