1234675d87a0ed3a3bd8fa0b55f7da9997e4119b
Cody Sheridan <csheridan994@gmail.com>
2018-04-29 15:28:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,15 +47,12 @@ function removeFromCart(item) {
    for (var i = 0; i < cart.length; i++) {
       if(cart[i].itemName === item)
         cart.splice(i, 1);
+        return cart;
       }
-      return cart;
+      return "That item is not in your cart.";
    }
 
-   
-
-
-
-
+  
 function placeOrder(cardNumber) {
   if (arguments[0] == undefined) {
     return "Sorry, we don't have a credit card on file for you."
