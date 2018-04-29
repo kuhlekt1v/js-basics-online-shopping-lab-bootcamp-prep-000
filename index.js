0dd0330ba32fb4811ac546ca254beff9cd67a140
Cody Sheridan <csheridan994@gmail.com>
2018-04-29 15:14:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function viewCart() {
 function total() {
  var total = 0;
  for ( var i = 0; i < cart.length; i++) {
-   total += cart[i].item.Price;
+   total += cart[i].itemPrice;
  }
  return total;
 }
